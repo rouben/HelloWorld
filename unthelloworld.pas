6701d55bb4ac85a9b048b7018cf63e036f53bcc5
Rouben Tchakhmakhtchian <rouben.tchakhmakhtchian@utoronto.ca>
2017-10-03 15:33:32 -0400

    Added OS version feature - untested on Windows

--- unthelloworld.pas
+++ unthelloworld.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, sqlite3conn, FileUtil, Forms, Controls, Graphics, Dialogs,
-  StdCtrls, LCLType, ComCtrls;
+  StdCtrls, LCLType, ComCtrls, Dos;
 
 type
 
@@ -15,6 +15,8 @@ type
   TfrmMain = class(TForm)
     AlphaTrackBar: TTrackBar;
     blbTrackBarInfo: TLabel;
+    lblVersionValue: TLabel;
+    lblVersion: TLabel;
     lblCurrentAlphaInfo: TLabel;
     lblAlphaValue: TLabel;
     procedure AlphaTrackBarChange(Sender: TObject);
@@ -36,6 +38,38 @@ implementation
 
 { TfrmMain }
 
+function OSVersion: String;
+ var
+  osErr: integer;
+  response: longint;
+  SystemStem, MajVer, MinVer: String;
+begin
+  {$IFDEF LCLcarbon}
+  OSVersion := 'Mac OS X 10.' + IntToStr(Lo(DosVersion) - 4) + '.' + IntToStr(Hi(DosVersion));
+  {$ELSE}
+  {$IFDEF Linux}
+  OSVersion := 'Linux Kernel ' + IntToStr(Lo(DosVersion) - 4) + '.' + IntToStr(Hi(DosVersion));
+  {$ELSE}
+  {$IFDEF UNIX}
+  OSVersion := 'Unix ' + IntToStr(Lo(DosVersion) - 4) + '.' + IntToStr(Hi(DosVersion));
+  {$ELSE}
+  {$IFDEF WINDOWS}
+  if WindowsVersion = wv95 then OSVersion := 'Windows 95 '
+   else if WindowsVersion = wvNT4 then OSVersion := 'Windows NT v.4 '
+   else if WindowsVersion = wv98 then OSVersion := 'Windows 98 '
+   else if WindowsVersion = wvMe then OSVersion := 'Windows ME '
+   else if WindowsVersion = wv2000 then OSVersion := 'Windows 2000 '
+   else if WindowsVersion = wvXP then OSVersion := 'Windows XP '
+   else if WindowsVersion = wvServer2003 then OSVersion := 'Windows Server 2003 '
+   else if WindowsVersion = wvVista then OSVersion := 'Windows Vista '
+   else if WindowsVersion = wv7 then OSVersion := 'Windows 7 '
+   else OSVersion:= 'Windows ' + IntToStr(Win32MajorVersion) + IntToStr(Win32MinorVersion);
+  {$ENDIF}
+  {$ENDIF}
+  {$ENDIF}
+  {$ENDIF}
+end;
+
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
    { Start off as 100% opaque }
@@ -44,10 +78,13 @@ begin
    frmMain.AlphaBlendValue := 255;
 end;
 
+
+
 procedure TfrmMain.AlphaTrackBarChange(Sender: TObject);
 begin
    lblAlphaValue.Caption := IntToStr(AlphaTrackBar.Position);
    frmMain.AlphaBlendValue := AlphaTrackBar.Position;
+   lblVersionValue.Caption := OSVersion;
 end;
 
 
